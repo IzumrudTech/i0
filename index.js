78856cd38496afa844366ed6b44972e4f2e493da
Алексей <a@guava.by>
2016-05-30 23:06:27 +0300

    fix help.js, remove try-catch from index.js, patch++

--- index.js
+++ index.js
@@ -7,12 +7,4 @@ const CLI = new CommandLineInterface();
 const globalHelp = require('./utils/globalHelp');
 
 CLI.load(__dirname, './commands');
-try{
-	CLI.run(process.argv.slice(2));
-} catch(e) {
-	if (e.message === "Cannot read property 'split' of undefined") {
-		console.log(globalHelp());
-	} else {
-		console.error(e);
-	}
-}
+CLI.run(process.argv.slice(2));
